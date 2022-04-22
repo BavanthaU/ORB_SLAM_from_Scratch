0fd3e56bb8aae76f19267f36970efe96017ca686
Bavantha <70237645+BavanthaU@users.noreply.github.com>
2022-04-22 07:14:42 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+import cv2
+import numpy as np
+
+# 1.07.13
+from Extractor import Extractor
+
+W = 1920 // 2
+H = 1080 // 2
+
+cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+fe = Extractor()
+
+
+def process_frame(img):
+    img = cv2.resize(img, (W, H))
+    matches = fe.extract(img)
+    if matches is None:
+        return
+    print("Number of Matching points : ", len(matches))
+    for pt1, pt2 in matches:
+        u1, v1 = map(lambda x: int(round(x)), pt1)
+        u2, v2 = map(lambda x: int(round(x)), pt2)
+        # print(p.pt,"-->",u, v)
+        cv2.circle(img, (u1, v1), color=(0, 255, 0), radius=3)
+        cv2.line(img, (u1, v1), (u2, v2), color=(255, 0, 0))
+
+    cv2.imshow("image", img)
+    cv2.waitKey(1)
+
+
+if __name__ == "__main__":
+    cap = cv2.VideoCapture("test_countryroad.mp4")
+
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret:
+            process_frame(frame)
+        else:
+            break
